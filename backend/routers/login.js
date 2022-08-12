a3c68346ab38226afe7441fe61e57452a566def4
souvikpal2000 <spal62588@gmail.com>
2022-08-12 10:55:54 +0530

    Login Page Functionality Done

--- backend/routers/login.js
+++ backend/routers/login.js
@@ -11,15 +11,15 @@ router.post("/login", async (req,res) => {
     try{
         const checkEmail = await User.findOne({email: email});
         if(!checkEmail){
-            return res.json({message: "This Email is not Registered"});
+            return res.json({status: 404, message: "This Email is not Registered"});
         }
         const user = await User.findOne({email: email, password: password});
         if(user){
             const token = jwt.sign({ ...user }, process.env.SECRET_KEY);
             res.cookie("jwt", token, { expires:new Date(Date.now() + 25892000000), httpOnly:true });
-            return res.status(200).json({message: "LoggedIn Successfully"});
+            return res.status(200).json({status: 200, message: "LoggedIn Successfully"});
         }
-        res.json({message: "Invalid Credentials"});
+        res.json({status: 401, message: "Invalid Credentials"});
     }catch(err){
         console.log(err);
     }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,10 +1,98 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import loginPic from "../images/login.jpg";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const [loginValues, setLoginValues] = useState({
+        email: "",
+        password: ""
+    });
+    const [alert,setAlert] = useState({
+        text: "",
+        status: ""
+    });
+    const navigate = useNavigate();
+
+    const inputValues = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setLoginValues((preValue) => {
+            return { 
+                ...preValue,
+                [name]: value
+            }
+        })
+    }
+
+    const submit = async (e) => {
+        e.preventDefault();
+        const res = await fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(loginValues)
+        })
+        const data = await res.json();
+        console.log(data);
+        if(data.status === 200){
+            setLoginValues({
+                email: "",
+                password: ""
+            });
+            navigate("/");
+        }else{
+            setAlert({
+                text: data.message,
+                status: "alert-danger"
+            })
+        }
+    }
+
     return(
         <>
             <div className="bodyContainer">
-                <h2>Login Page</h2>
+                <Container>
+                    <Row>
+                        <Col className="alignItem col-12 col-md-6">
+                            <img src={loginPic} alt="" className="loginImage"/>
+                        </Col>
+                        <Col className="alignItem col-12 col-md-6">
+                            {alert.text && 
+                            <div className={'alert' + ' ' + alert.status} role="alert">
+                                <p>{alert.text}</p>
+                                <button class="btn-close" aria-label="Close" onClick={() => setAlert("")}></button>
+                            </div>}
+                            <form method="POST" className="form" onSubmit={submit}>
+                                <div className="form-group row justify-content-center">
+                                    <div className="col-12 col-md-10">
+                                        <div className="input-group mb-3">
+                                            <span className="input-group-text" id="basic-addon1">Email</span>
+                                            <input type="email" name="email" id="email" className="form-control" required value={loginValues.email} onChange={inputValues}/>
+                                        </div>
+                                    </div>
+                                </div>
+                
+                                <div className="form-group row justify-content-center">
+                                    <div className="col-12 col-md-10">
+                                        <div className="input-group mb-3">
+                                            <span className="input-group-text" id="basic-addon1">Password</span>
+                                            <input type="password" name="password" id="password" className="form-control" required value={loginValues.password} onChange={inputValues}/>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="form-group row justify-content-center">
+                                    <button type="submit" className="btn btn-success">Login</button>
+                                </div>             
+                            </form>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </>
     )
