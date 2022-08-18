61ff2f6a70b3f53e0b627567215ebc253802d773
souvikpal2000 <spal62588@gmail.com>
2022-08-18 09:32:40 +0530

    404 Error Page

--- frontend/src/components/Error.jsx
+++ frontend/src/components/Error.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Common from "./common/Common";
 
 const Error = () => {
     return(
         <>
+            <Common/>
             <div className="bodyContainer">
                 <h1>404 Error</h1>
                 <h3>Page not Found!!!</h3>
