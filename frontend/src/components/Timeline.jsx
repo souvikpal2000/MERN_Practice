9a2b47f1af9cef383f1235f3c5409384c89df42b
souvikpal2000 <spal62588@gmail.com>
2022-08-26 10:54:33 +0530

    Modal Added

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -1,10 +1,52 @@
 import React, { useContext } from "react";
 import { UserValueContext } from "./About";
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from "react";
 
 const Timeline = () => {
     const {state, setState} = useContext(UserValueContext);
     const {messages} = state.data;
+
+    const [model, setModel] = useState({
+        id: "",
+        show: false
+    });
+    const showModel = (e) => {
+        const id = e.target.id;
+        setModel({
+            id: id,
+            show: true
+        });
+    }
+    const closeModel = () => {
+        setModel({
+            id: "",
+            show: false
+        });
+    }
+    const VerticallyAlignedModel = () => {
+        return(
+            <>
+                <Modal show={model.show} onHide={closeModel} backdrop="static" keyboard={false} centered>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Delete Message?</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        You'll lose this message after this action. We can't recover them once you delete.
+                        Are you sure you want to <span class="perDlt">permanently delete</span> it?
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={closeModel}>
+                            Close
+                        </Button>
+                        <Button variant="danger" id={model.id} onClick={deleteMessage}>Yes</Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+        );
+    }
 
     const deleteMessage = (e) => {
         const updatedMessages = messages.filter((msg, index) => {
@@ -16,6 +58,10 @@ const Timeline = () => {
                 ...preValue,
                 data: updatedData
             }
+        });
+        setModel({
+            id: "",
+            show: false
         })
     }
 
@@ -31,13 +77,14 @@ const Timeline = () => {
                                     <Card.Text className="description">{msg.desc}</Card.Text>
                                 </div>
                                 <div className="trashContent">
-                                    <p id={index} onClick={deleteMessage} className="cross">❌</p>
+                                    <p id={index} onClick={showModel} className="cross">❌</p>
                                 </div>
                             </Card.Body>
                         </Card>
                     )
                 })}
             </div>
+            <VerticallyAlignedModel/>
         </>
     )
 }
