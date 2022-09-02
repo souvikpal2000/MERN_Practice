b087affa2ef90be2c28ba59c3b9eac57c667db03
souvikpal2000 <spal62588@gmail.com>
2022-09-02 10:09:59 +0530

    Contact API Fixed for not LoggedIn Users

--- backend/routers/contact.js
+++ backend/routers/contact.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const auth = require("../middleware/auth");
 const { Reply, Message, User } = require("../db/models/users");
+const { UnRegUserMessage, UnRegUser } = require("../db/models/unreg_users");
 const router = express.Router();
 
 router.get("/contact", auth, (req, res) => {
@@ -11,9 +12,35 @@ router.post("/contact", async (req, res) => {
     const {email} = req.body;
     try{
         const user = await User.findOne({email: email});
+
+        if(!user){
+            const data = await UnRegUser.findOne({email: email});
+
+            const message = new UnRegUserMessage({
+                desc: req.body.message
+            })
+
+            if(!data){
+                const userData = new UnRegUser({
+                    name: req.body.name,
+                    email: req.body.email,
+                    phone: req.body.phone
+                });
+                const saveUserData = await userData.save();
+                saveUserData.messages.push(message);
+                await saveUserData.save();
+                return res.json({status: 200, message: "Message Sent Successfully"});
+            }
+
+            data.messages.push(message);
+            await data.save();
+            return res.json({status: 200, message: "Message Sent Successfully"});
+        }
+
         const message = new Message({
             desc: req.body.message
         });
+
         user.messages.push(message);
         await user.save();
         res.json({status: 200, message: "Message Sent Successfully"});
