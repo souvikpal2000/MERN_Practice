34e45b517fff50206008bcbcbc195782bb784ff5
souvikpal2000 <spal62588@gmail.com>
2022-09-22 10:47:39 +0530

    Modal Added for Deletion Confirmation

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,19 +1,22 @@
-import React, { useEffect, useState, createContext } from "react";
+import React, { useEffect, useState, createContext, useContext } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Card from 'react-bootstrap/Card';
 import Spinner from "react-bootstrap/Spinner";
-import { useContext } from "react";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
-const ReplyContext = createContext();
+const ModelContext = createContext();
 
 const ReplyCard = ({messageId, replyId, ...reply}) => {
-    const {replies, setReplies} = useContext(ReplyContext);
-    const deleteReply = (e) => {
-        const updatedReplies = replies.filter((reply, index) => {
-            return index != e.target.id
-        });
-        setReplies(updatedReplies);
+    const {setModel, user} = useContext(ModelContext);
+    const openModal = () => {
+        setModel({
+            show: true,
+            email: user.email,
+            messageId: messageId,
+            replyId: replyId
+        })
     }
 
     return(
@@ -25,7 +28,7 @@ const ReplyCard = ({messageId, replyId, ...reply}) => {
                         <Card.Text className="description">{reply.desc}</Card.Text>
                     </div>
                     <div className="trashContent">
-                        <p className="cross" id={replyId} onClick={deleteReply}>❌</p>
+                        <p className="cross" id={replyId} onClick={openModal}>❌</p>
                     </div>
                 </Card.Body>
             </Card>
@@ -34,7 +37,6 @@ const ReplyCard = ({messageId, replyId, ...reply}) => {
 }
 
 const MessageCard = ({messageId, ...message}) => {
-    const [replies, setReplies] = useState(message.replies);
     const [showReplies, setShowReplies] = useState(false);
     return(
         <>
@@ -46,7 +48,7 @@ const MessageCard = ({messageId, ...message}) => {
                     </div>
                     <div className="trashContent">
                         <button className="btn btn-secondary giveReply">Give Reply</button>
-                        {replies.length>0? 
+                        {message.replies.length>0? 
                         <div>
                             {showReplies? <p className="minus" onClick={() => setShowReplies(!showReplies)}>➖</p> : <p className="plus" onClick={() => setShowReplies(!showReplies)}>➕</p>}
                         </div> : null}
@@ -55,22 +57,83 @@ const MessageCard = ({messageId, ...message}) => {
             </Card>
             {showReplies? 
             <div className="replyContainer">
-                <ReplyContext.Provider value={{replies, setReplies}}>
-                    {replies.map((reply, index) => {
-                        return(
-                            <ReplyCard key={index} messageId={messageId} replyId={index} {...reply}/>
-                        )
-                    })}
-                </ReplyContext.Provider>
+                {message.replies.map((reply, index) => {
+                    return(
+                        <ReplyCard key={index} messageId={messageId} replyId={index} {...reply}/>
+                    )
+                })}
             </div> : null}
         </>
     )
 }
 
+const VerticallyAlignedModel = () => {
+    const {model, setModel, user, setUser} = useContext(ModelContext);
+
+    const closeModel = () => {
+        setModel({
+            show: false,
+            email: "",
+            messageId: "",
+            replyId: ""
+        })
+    }
+    
+    const deleteMessage = () => {
+        const {messages} = user;
+        const {replies} = messages[model.messageId];
+        const updatedReplies = replies.filter((reply, index) => {
+            return index != model.replyId
+        });
+        messages[model.messageId].replies = updatedReplies;
+        setUser((preValue) => {
+            return{
+                ...preValue,
+                messages: messages
+            }
+        });
+        setModel({
+            show: false,
+            email: "",
+            messagesId: "",
+            replyId: ""
+        })
+    }
+
+    return(
+        <>
+            <Modal show={model.show} onHide={closeModel} backdrop="static" keyboard={false} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Delete Message?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    You'll lose this message after this action. We can't recover them once you delete.
+                    Are you sure you want to <span className="perDlt">permanently delete</span> it?
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={closeModel}>
+                        Close
+                    </Button>
+                    <Button variant="danger" onClick={deleteMessage}>Yes</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
 const Messages = () => {
     const location = useLocation();
-    const [messages, setMessages] = useState([]);
+    const [user, setUser] = useState({
+        email: "",
+        messages: []
+    });
     const [spinner, setSpinner] = useState(true);
+    const [model, setModel] = useState({
+        show: false,
+        email: "",
+        messageId: "",
+        replyId: ""
+    });
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -83,7 +146,10 @@ const Messages = () => {
             credentials: "include"
         }).then(async (res) => {
             const data = await res.json();
-            setMessages(data.messages);
+            setUser({
+                email: data.user.email,
+                messages: data.user.messages
+            });
             closeSpinnerIn1Seconds();
         }).catch((err) => {
             console.log(err);
@@ -98,27 +164,31 @@ const Messages = () => {
 
     return(
         <>
-            {spinner? 
-            <Container className="noMessageContainer">
-                <Spinner animation="border" variant="primary" />
-            </Container> : messages.length>0?
-            <Container className="messageContainer">
-                <div className="messageHeading">
-                    <h2>Messages</h2>
-                    <button className="btn btn-primary goBack" onClick={() => navigate("/admin")}>Go Back</button>
-                </div>
-                <div className="messages">
-                    {messages.map((message, index) => {
-                        return(
-                            <MessageCard key={index} messageId={index} {...message}/>
-                        )
-                    })}
-                </div>
-            </Container>:
-            <Container className="noMessageContainer">
-                <h1>No Messages</h1>
-                <button className="btn btn-primary" onClick={() => navigate("/admin")}>Go Back</button>
-            </Container>}
+            <ModelContext.Provider value={{model, setModel, user, setUser}}>
+                {spinner? 
+                <Container className="noMessageContainer">
+                    <Spinner animation="border" variant="primary" />
+                </Container> : user.messages.length>0?
+                <Container className="messageContainer">
+                    <div className="messageHeading">
+                        <h2>Messages</h2>
+                        <button className="btn btn-primary goBack" onClick={() => navigate("/admin")}>Go Back</button>
+                    </div>
+                    <div className="messages">
+                        {user.messages.map((message, index) => {
+                            return(
+                                <MessageCard key={index} messageId={index} {...message}/>
+                            )
+                        })}
+                    </div>
+                </Container>:
+                <Container className="noMessageContainer">
+                    <h1>No Messages</h1>
+                    <button className="btn btn-primary" onClick={() => navigate("/admin")}>Go Back</button>
+                </Container>}
+            
+                <VerticallyAlignedModel/>
+            </ModelContext.Provider>
         </>
     )
 }
