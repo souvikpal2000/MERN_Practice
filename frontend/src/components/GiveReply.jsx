29ddf35e65d243cf74f88f7e0cce873a5546acd9
souvikpal2000 <spal62588@gmail.com>
2022-09-26 11:31:23 +0530

    API to get User's Particular Message is Fetched

--- frontend/src/components/GiveReply.jsx
+++ frontend/src/components/GiveReply.jsx
@@ -1,24 +1,49 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import Container from "react-bootstrap/Container";
+import { useLocation, useNavigate } from "react-router-dom";
 import GiveReplyPic from "../images/giveReply.jpg";
 
 const GiveReply = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const email = location.pathname.substring(12, location.pathname.lastIndexOf("/"));
+
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch(location.pathname, {
+            method: "GET", 
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            credentials: "include"
+        }).then(async (res) => {
+            const data = await res.json();
+            if(data.status === "200"){
+                setMessage(data.message);
+            }
+        })
+    }, []);
+
     return(
         <>
             <Container>
                 <div className="messageHeading">
                     <h2>Give Reply</h2>
-                    <button className="btn btn-primary goBack">Go Back</button>
+                    <button className="btn btn-primary goBack" onClick={() => navigate(`/admin/viewmsg/${email}`)}>Go Back</button>
                 </div>
                 <div className="replyFormContainer">
                     <div className="alignItem">
                         <img src={GiveReplyPic} alt="" className="giveReplyImage"/>
                     </div>
                     <div className="alignItem">
-                        <form className="">
-                        <div class="form-group">
+                        <form>
+                            <div class="form-group mb-2">
                                 <label for="reply" className="reply">Message</label>
-                                <p></p>
+                                <textarea name="reply" id="reply" class="form-control" rows={3} disabled defaultValue={message}></textarea>
                             </div>
                             <div class="form-group">
                                 <label for="reply" className="reply">Reply</label>
