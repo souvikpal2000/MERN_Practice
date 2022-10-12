58c90687ac2c68447aab7d32bc786682406930cd
souvikpal2000 <spal62588@gmail.com>
2022-10-12 09:48:24 +0530

    Created API to Update User Info

--- backend/routers/about.js
+++ backend/routers/about.js
@@ -21,6 +21,17 @@ router.delete("/timeline/:id", auth, async (req,res) => {
     }catch(err){
         res.json({status: 500});
     }
+});
+
+router.post("/about", auth, async (req,res) => {
+    const {id} = req.rootUser;
+    try{
+        const {name, phone, work} = req.body;
+        await User.findOneAndUpdate({_id: id}, {$set: {name: name, phone: phone, work: work}}, {new: true});
+        res.json({status: 200});
+    }catch(err){
+        res.json({status: 500});
+    }
 })
 
 module.exports = router;
